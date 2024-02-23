10946a4a5bb29b9fda2b0bcde2db983613f9bed6
Nil Borodulia <nil.borodulia@gmail.com>
2024-02-23 12:52:13 +0300

    Update message formatting with status emojis

--- app/sender/sender.go
+++ app/sender/sender.go
@@ -38,10 +38,10 @@ func NewSender(senderType string, message Message) Sender {
 
 func getTextMessage(message Message) string {
 	if message.Response.Err != nil {
-		return fmt.Sprintf("*Service:* %s\n*Status:* %s\n*Datetime:* %s\n*URL:* %s\n*Error:* %s",
+		return fmt.Sprintf("❗*Service:* %s\n*Status:* %s\n*Datetime:* %s\n*URL:* %s\n*Error:* %s",
 			message.ServiceName, message.Status, message.Datetime, message.Url, message.Response.Err.Error())
 	}
 
-	return fmt.Sprintf("*Service:* %s\n*Status:* %s\n*Datetime:* %s\n*URL:* %s",
+	return fmt.Sprintf("✅ *Service:* %s\n*Status:* %s\n*Datetime:* %s\n*URL:* %s",
 		message.ServiceName, message.Status, message.Datetime, message.Url)
 }
