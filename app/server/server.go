6f6649e05350b74817e4b425117c439caf426f4f
Nil Borodulia <nil.borodulia@gmail.com>
2024-04-21 20:13:54 +0200

    Add new ReloadConfigMiddleware function

--- app/server/server.go
+++ app/server/server.go
@@ -86,22 +86,3 @@ func (s Server) routes() chi.Router {
 
 	return router
 }
-
-func ReloadConfigMiddleware(cfg config.Config) func(next http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			config, err := config.LoadConfig("config.yml") // Load config from file
-			if err != nil {
-				log.Printf("[ERROR] failed to load config, %v", err)
-				render.Status(r, http.StatusInternalServerError)
-				render.JSON(w, r, `{Message: "failed to load config"}`)
-				return
-			}
-			r = r.WithContext(context.WithValue(r.Context(), "config", config))
-			cfg = config
-			log.Printf("[INFO] config reloaded")
-			//
-			next.ServeHTTP(w, r)
-		})
-	}
-}
